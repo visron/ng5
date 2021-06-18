13f7ba4a8a59a4852223c6eef487c4cf287032d8
vis <hdronoh@gmail.com>
2021-06-18 23:55:14 +0300

    Services Using and sharing data in different components. Create a dependency injection and declare an observable in the service.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+import { DataService } from "../data.service";
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -32,20 +34,27 @@ export class HomeComponent implements OnInit {
   itemCount : number=2;
   btnText: string = "Add new item";
   goalText:string = "My first goal";
-  goals : string[]=['2m','20 houses','4 cars'];
-  constructor() { }
+  goals : string[]=[];
+  constructor(private _data:DataService) { }
 
   ngOnInit(): void {
+    this._data.goal.subscribe(res=>this.goals=res);
     this.itemCount = this.goals.length;
+
+    this._data.changeGoal(this.goals);
   }
   addItem(){
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
+
 
   }
   removeItem(i:number){
     this.goals.splice(i,1);
+    this._data.changeGoal(this.goals);
+
   }
 
 }
